161f3d93a427b6b084c7827d8031c73bc0f6f235
Steven Liao <steven_y_liao@brown.edu>
2023-11-27 15:00:08 -0500

    new

--- client/src/components/NodeView/NodeContent/NodeContent.tsx
+++ client/src/components/NodeView/NodeContent/NodeContent.tsx
@@ -5,6 +5,7 @@ import { FolderContent } from "./FolderContent";
 import { ImageContent } from "./ImageContent";
 import "./NodeContent.scss";
 import { TextContent } from "./TextContent";
+import { RecipeContent } from "./RecipeContent/RecipeContent";
 
 /** Props needed to render any node content */
 
@@ -29,6 +30,8 @@ export const NodeContent = (props: INodeContentProps) => {
     case "text":
       return <TextContent nodeIdsToNodesMap={nodeIdsToNodesMap} />;
       break;
+    case "recipe":
+      return <RecipeContent />;
     case "folder":
       if (childNodes) {
         return (
---
+++ client/src/components/NodeView/NodeContent/RecipeContent/RecipeContent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RecipeContent = () => {
+  return <div>RecipeContent</div>;
+};
