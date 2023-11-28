50c252a78f5696ed824a860b7373006e80acae70
Steven Liao <steven_y_liao@brown.edu>
2023-11-28 16:06:40 -0500

    new

--- client/src/components/NodeView/NodeContent/RecipeContent/RecipeContent.tsx
+++ client/src/components/NodeView/NodeContent/RecipeContent/RecipeContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as ri from "react-icons/ri";
 import * as ai from "react-icons/ai";
 import { LuUtensils } from "react-icons/lu";
@@ -7,9 +7,27 @@ import { useRecoilState } from "recoil";
 import { currentNodeState } from "~/global/Atoms";
 import "./RecipeContent.scss";
 import { IRecipeNode } from "~/types";
+import convert from "convert";
+import {
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+} from "@chakra-ui/react";
 
 export const RecipeContent = () => {
   const [currentNode] = useRecoilState(currentNodeState);
+  const [tempUnits] = useState(["celsius", "fahrenheit", "kelvin"]);
+  const [selectedUnitType, setSelectedUnitType] = useState("");
+  useEffect(() => {
+    console.log((currentNode as IRecipeNode).description.content);
+    console.log(convert(1, "kelvin").to("fahrenheit"));
+    console.log(selectedUnitType);
+  }, [selectedUnitType]);
+
+  const handleUnitTypeChange = () => {};
   return (
     <div className="recipe-container">
       <div className="recipe-left">
@@ -36,11 +54,41 @@ export const RecipeContent = () => {
             </p>
           </div>
         </div>
+        <div className="unit-converter">
+          <Select onChange={handleUnitTypeChange} variant={"filled"}>
+            <option value="Volume">Volume</option>
+            <option value="Weight">Weight</option>
+            <option value="Temperature" selected>
+              Temperature
+            </option>
+          </Select>
+          <div className="input-section">
+            <div className="left-input">
+              <NumberInput style={{ width: "90%" }} defaultValue={1}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </div>
+            <b>=</b>
+            <div className="right-input">
+              <NumberInput style={{ width: "90%" }} defaultValue={15}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="recipe-right">
         <div className="recipe-description-container">
           <b style={{ fontSize: 30 }}>Description</b>
-          <div>{(currentNode as IRecipeNode).description}</div>
+          <div>{(currentNode as IRecipeNode).description.content}</div>
         </div>
         <div className="recipe-ingredients-container"></div>
       </div>
