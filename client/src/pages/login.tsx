f9a222ded6f38952ba4c1f0bedf2ea51c9293586
alex-lin64 <alex_lin@brown.edu>
2023-12-02 22:10:07 -0500

    sdf

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,13 @@
-import Login from "~/components/Login/Login";
+import { getServerSession } from "next-auth";
+import LoginForm from "~/components/Login/LoginForm";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function Register() {
-  return <Login />;
+export default function Login() {
+  // const session = await getServerSession(authOptions);
+
+  // if (session) redirect("/dashboard");
+
+  return <LoginForm />;
 }
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,5 +1,17 @@
 import RegisterForm from "~/components/RegisterForm/RegisterForm";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Register() {
+  // useEffect(() => {
+  //   const checkSession = async () => {
+  //   }
+  //   const session = await getServerSession(authOptions);
+  // }, []);
+
+  // if (session) redirect("/dashboard");
+
   return <RegisterForm />;
 }
