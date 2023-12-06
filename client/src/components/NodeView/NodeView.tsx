ab16b5379d4e889d063bc8257442142f141c4388
Steven Liao <steven_y_liao@brown.edu>
2023-12-06 16:20:35 -0500

    new

--- client/src/components/NodeView/NodeView.tsx
+++ client/src/components/NodeView/NodeView.tsx
@@ -77,7 +77,6 @@ export const NodeView = (props: INodeViewProps) => {
   setCurrentNode(currentNode);
 
   const loadAnchorsFromNodeId = useCallback(async () => {
-    console.log("bruh");
     const anchorsFromNode = await FrontendAnchorGateway.getAnchorsByNodeId(
       currentNode.nodeId
     );
@@ -87,26 +86,21 @@ export const NodeView = (props: INodeViewProps) => {
   }, [currentNode]);
 
   const loadChildAnchorsFromNodeId = useCallback(async () => {
-    setAnchors([]);
+    const newAnchors: IAnchor[] = [];
     const nodeIDs: string[] = [
       (currentNode as IRecipeNode).descriptionID,
       (currentNode as IRecipeNode).ingredientsID,
       (currentNode as IRecipeNode).stepsID,
     ];
-    console.log(nodeIDs);
     nodeIDs.forEach(async (nodeID) => {
       const anchorsFromNode = await FrontendAnchorGateway.getAnchorsByNodeId(
         nodeID
       );
-      console.log(nodeID);
       if (anchorsFromNode.success) {
-        console.log(anchorsFromNode.payload);
-        const newAnchors = [...anchors]; // Create a shallow copy of the existing anchors
-        newAnchors.push(...anchorsFromNode.payload); // Concatenate the new anchors
-        setAnchors(newAnchors); // Update the state
+        newAnchors.push(...anchorsFromNode.payload);
       }
     });
-    console.log(anchors, "finalAnchors");
+    setAnchors(newAnchors);
   }, [currentNode]);
 
   const handleStartLinkClick = () => {
@@ -167,7 +161,10 @@ export const NodeView = (props: INodeViewProps) => {
   useEffect(() => {
     if (currentNode.type == "recipe") {
       console.log("recipeNode");
+      console.log(anchors, "anchorsBefore");
       loadChildAnchorsFromNodeId();
+      console.log(anchors, "anchorsAfter");
+      console.log(hasAnchors);
     }
   }, [loadChildAnchorsFromNodeId, currentNode, refresh, setSelectedAnchors]);
 
@@ -175,8 +172,6 @@ export const NodeView = (props: INodeViewProps) => {
   const hasAnchors: boolean = anchors.length > 0;
   let nodePropertiesWidth: number = hasAnchors ? 200 : 0;
   const nodeViewWidth = `calc(100% - ${nodePropertiesWidth}px)`;
-
-  console.log(hasAnchors, "hasAnchors", anchors);
 
   const nodeProperties = useRef<HTMLHeadingElement>(null);
   const divider = useRef<HTMLHeadingElement>(null);
